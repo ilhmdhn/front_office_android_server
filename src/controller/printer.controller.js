45f774d12411ee5ff892b0c1f26847d5941f3080
Ilham Dohaan <doha39anilham@gmail.com>
2022-07-01 17:12:09 +0700

    add escpos

--- src/controller/printer.controller.js
+++ src/controller/printer.controller.js
@@ -1,22 +1,62 @@
 var ResponseFormat = require('../util/response');
-const ThermalPrinter = require("node-thermal-printer").printer;
-const PrinterTypes = require("node-thermal-printer").types;
+const escpos = require('escpos');
+escpos.USB = require('escpos-usb');
+const device  = new escpos.USB();
 
 exports.testPrinter = async function (req, res){
     try{
-      var printerName = req.query.printer;
-        let printer = new ThermalPrinter({
-          type: PrinterTypes.EPSON,
-          interface: 'printer:'+printerName,
-          driver: require('@thiagoelg/node-printer')
-        });
-        
-        await printer.raw(Buffer.from("Hello world"));
-        await printer.raw(Buffer.from("Hello world"));
-        await printer.raw(Buffer.from("Hello world"));
 
-        console.log('tes koneksi ' + await printer.isPrinterConnected())
-        res.send(new ResponseFormat(true, null, "beerhasil Print To " +printerName))
+      // const options = { encoding: "GB18030" /* default */ }
+ 
+      // const printer = new escpos.Printer(device, options); 
+      // device.open(function(error){
+      //   if(error){
+      //     console.log(error)
+      //     console.log(error.message)
+      //   } else{
+      //     printer
+      //     .font('a')
+      //     .align('ct')
+      //     .style('NORMAL')
+      //     .size(1,1)
+      //     .text('BLACKHOLE')
+      //     .text('Lenmarc Lt.3 A11')
+      //     .text('SURABAYA')
+      //     .text('031 1163151')
+      //     .newLine()
+      //     .style('B')
+      //     .text('TAGIHAN')
+      //     .newLine()
+      //     .newLine()
+      //     .newLine()
+      //     .newLine()
+      //     .table(
+      //       ["Kiri", "", "Kanan"]
+      //     )
+      //     .tableCustom(
+      //       [
+      //         { text:"Kiri", align:"LEFT"},
+      //         { text:"Kanan", align:"RIGHT"}
+      //       ],)
+      //       .tableCustom(
+      //         [
+      //           { text:"Left", align:"LEFT", width:0.33, style: 'B' },
+      //           { text:"Center", align:"CENTER", width:0.33},
+      //           { text:"Right", align:"RIGHT", width:0.33 }
+      //         ],
+      //         { encoding: 'cp857', size: [1, 1] } // Optional
+      //       )
+      //     .text('telo')
+      //     .newLine()
+      //     .newLine()
+      //     .newLine()
+      //     .newLine()
+      //     .cut()
+      //     .close();
+      //   }
+      // });
+
+        res.send(new ResponseFormat(true, null, "berhasil Print To "))
       } catch(error){
         console.log('Error')
         console.log(error)
